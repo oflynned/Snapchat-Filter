2e40fb43f36df2256bc9fd07160365a7e8388c4e
Ed <oflynned@tcd.ie>
2017-03-06 20:48:05 +0000

    added moustache filter that is more stable than using mouth points alone - utilises eyes for stability

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import dlib
 import numpy as np
 import math
+import os
 
 from video import create_capture
 
@@ -135,46 +136,28 @@ def get_face_mask(im, landmarks):
 
 
 def transformation_from_points(points1, points2):
-    """
-    Return an affine transformation [s * R | T] such that:
-        sum ||s*R*p1,i + T - p2,i||^2
-    is minimized.
-    """
-    # Solve the procrustes problem by subtracting centroids, scaling by the
-    # standard deviation, and then using the SVD to calculate the rotation. See
-    # the following for more details:
-    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
-
     points1 = points1.astype(np.float64)
-    points2 = points2.astype(np.float64)
-
     c1 = np.mean(points1, axis=0)
-    c2 = np.mean(points2, axis=0)
     points1 -= c1
-    points2 -= c2
-
     s1 = np.std(points1)
-    s2 = np.std(points2)
     points1 /= s1
+
+    points2 = points2.astype(np.float64)
+    c2 = np.mean(points2, axis=0)
+    points2 -= c2
+    s2 = np.std(points2)
     points2 /= s2
 
-    U, S, Vt = np.linalg.svd(points1.T * points2)
+    u, s, vt = np.linalg.svd(points1.T * points2)
+    r = (u * vt).T
 
-    # The R we seek is in fact the transpose of the one given by U * Vt. This
-    # is because the above formulation assumes the matrix goes on the right
-    # (with row vectors) where as our solution requires the matrix to be on the
-    # left (with column vectors).
-    R = (U * Vt).T
-
-    return np.vstack([np.hstack(((s2 / s1) * R, c2.T - (s2 / s1) * R * c1.T)), np.matrix([0., 0., 1.])])
+    return np.vstack([np.hstack(((s2 / s1) * r, c2.T - (s2 / s1) * r * c1.T)), np.matrix([0., 0., 1.])])
 
 
 def read_im_and_landmarks(fname):
-    im = cv2.imread(fname, -1)
+    im = cv2.imread(fname, 1)
     im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR, im.shape[0] * SCALE_FACTOR))
-    s = get_landmarks(im)
-
-    return im, s
+    return im, get_landmarks(im)
 
 
 def warp_im(im, M, dshape):
@@ -224,128 +207,188 @@ def get_rotated_points(point, anchor, deg_angle):
     return [int(qx), int(qy)]
 
 
+def glasses_filter(cam, glasses):
+    face = get_cam_frame(cam)
+    landmarks = get_landmarks(face)
+
+    pts1 = np.float32([[0, 0], [599, 0], [0, 208], [599, 208]])
+
+    if type(landmarks) is not int:
+        """
+        GLASSES ANCHOR POINTS:
+
+        17 & 26 edges of left eye and right eye (left and right extrema)
+        0 & 16 edges of face across eyes (other left and right extra, interpolate between 0 & 17, 16 & 26 for half way points)
+        19 & 24 top of left and right brows (top extreme)
+        27 is centre of the eyes on the nose (centre of glasses)
+        28 is the bottom threshold of glasses (perhaps interpolate between 27 & 28 if too low) (bottom extreme)
+        """
+
+        left_face_extreme = [landmarks[0, 0], landmarks[0, 1]]
+        right_face_extreme = [landmarks[16, 0], landmarks[16, 1]]
+        x_diff_face = right_face_extreme[0] - left_face_extreme[0]
+        y_diff_face = right_face_extreme[1] - left_face_extreme[1]
+
+        face_angle = math.degrees(math.atan2(y_diff_face, x_diff_face))
+
+        # get hypotenuse
+        face_width = math.sqrt((right_face_extreme[0] - left_face_extreme[0]) ** 2 +
+                               (right_face_extreme[1] - right_face_extreme[1]) ** 2)
+        glasses_width = face_width * 1.0
+
+        # top and bottom of left eye
+        eye_height = math.sqrt((landmarks[19, 0] - landmarks[28, 0]) ** 2 +
+                               (landmarks[19, 1] - landmarks[28, 1]) ** 2)
+        glasses_height = eye_height * 1.2
+
+        # generate bounding box from the anchor points
+        anchor_point = [landmarks[27, 0], landmarks[27, 1]]
+        tl = [int(anchor_point[0] - (glasses_width / 2)), int(anchor_point[1] - (glasses_height / 2))]
+        rot_tl = get_rotated_points(tl, anchor_point, face_angle)
+
+        tr = [int(anchor_point[0] + (glasses_width / 2)), int(anchor_point[1] - (glasses_height / 2))]
+        rot_tr = get_rotated_points(tr, anchor_point, face_angle)
+
+        bl = [int(anchor_point[0] - (glasses_width / 2)), int(anchor_point[1] + (glasses_height / 2))]
+        rot_bl = get_rotated_points(bl, anchor_point, face_angle)
+
+        br = [int(anchor_point[0] + (glasses_width / 2)), int(anchor_point[1] + (glasses_height / 2))]
+        rot_br = get_rotated_points(br, anchor_point, face_angle)
+
+        pts = np.float32([rot_tl, rot_tr, rot_bl, rot_br])
+        m = cv2.getPerspectiveTransform(pts1, pts)
+
+        rotated = cv2.warpPerspective(glasses, m, (face.shape[1], face.shape[0]))
+        result_2 = blend_transparent(face, rotated)
+
+        for p in pts:
+            pos = (p[0], p[1])
+            cv2.circle(result_2, pos, 2, (0, 0, 255), 2)
+            cv2.putText(result_2, str(p), pos, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(255, 0, 0))
+
+        cv2.imshow("Glasses Filter", result_2)
+
+
+def moustache_filter(cam, moustache):
+    face = get_cam_frame(cam)
+    landmarks = get_landmarks(face)
+
+    pts1 = np.float32([[0, 0], [599, 0], [0, 208], [599, 208]])
+
+    """
+    MOUSTACHE ANCHOR POINTS
+
+    centre anchor point is midway between 34 (top of philtrum) and 54 (bottom of philtrum)
+    width can be determined by the eyes as the mouth can move
+    height also determined by the eyes as before
+    generate as before and just modify multiplier coefficients & translate to anchor point?
+
+
+    ^^^ mouth and jaw can move, use eyes as anchor point initially then translate to philtrum position
+    """
+
+    if type(landmarks) is not int:
+        left_face_extreme = [landmarks[0, 0], landmarks[0, 1]]
+        right_face_extreme = [landmarks[16, 0], landmarks[16, 1]]
+        x_diff_face = right_face_extreme[0] - left_face_extreme[0]
+        y_diff_face = right_face_extreme[1] - left_face_extreme[1]
+
+        face_angle = math.degrees(math.atan2(y_diff_face, x_diff_face))
+
+        # get hypotenuse
+        face_width = math.sqrt((right_face_extreme[0] - left_face_extreme[0]) ** 2 +
+                               (right_face_extreme[1] - right_face_extreme[1]) ** 2)
+        moustache_width = face_width * 0.8
+
+        # top and bottom of left eye
+        eye_height = math.sqrt((landmarks[19, 0] - landmarks[28, 0]) ** 2 +
+                               (landmarks[19, 1] - landmarks[28, 1]) ** 2)
+        glasses_height = eye_height * 0.8
+
+        # generate bounding box from the anchor points
+        brow_anchor = [landmarks[27, 0], landmarks[27, 1]]
+        tl = [int(brow_anchor[0] - (moustache_width / 2)), int(brow_anchor[1] - (glasses_height / 2))]
+        rot_tl = get_rotated_points(tl, brow_anchor, face_angle)
+
+        tr = [int(brow_anchor[0] + (moustache_width / 2)), int(brow_anchor[1] - (glasses_height / 2))]
+        rot_tr = get_rotated_points(tr, brow_anchor, face_angle)
+
+        bl = [int(brow_anchor[0] - (moustache_width / 2)), int(brow_anchor[1] + (glasses_height / 2))]
+        rot_bl = get_rotated_points(bl, brow_anchor, face_angle)
+
+        br = [int(brow_anchor[0] + (moustache_width / 2)), int(brow_anchor[1] + (glasses_height / 2))]
+        rot_br = get_rotated_points(br, brow_anchor, face_angle)
+
+        # locate new location for moustache on philtrum
+        top_philtrum_point = [landmarks[33, 0], landmarks[33, 1]]
+        bottom_philtrum_point = [landmarks[51, 0], landmarks[51, 1]]
+        philtrum_anchor = [(top_philtrum_point[0] + bottom_philtrum_point[0]) / 2,
+                           (top_philtrum_point[1] + bottom_philtrum_point[1]) / 2]
+
+        # determine distance from old origin to new origin and translate
+        anchor_distance = [int(philtrum_anchor[0] - brow_anchor[0]), int(philtrum_anchor[1] - brow_anchor[1])]
+        rot_tl[0] += anchor_distance[0]
+        rot_tl[1] += anchor_distance[1]
+        rot_tr[0] += anchor_distance[0]
+        rot_tr[1] += anchor_distance[1]
+        rot_bl[0] += anchor_distance[0]
+        rot_bl[1] += anchor_distance[1]
+        rot_br[0] += anchor_distance[0]
+        rot_br[1] += anchor_distance[1]
+
+        pts = np.float32([rot_tl, rot_tr, rot_bl, rot_br])
+        m = cv2.getPerspectiveTransform(pts1, pts)
+
+        rotated = cv2.warpPerspective(moustache, m, (face.shape[1], face.shape[0]))
+        result_2 = blend_transparent(face, rotated)
+
+        # annotate_landmarks(result_2, landmarks)
+
+        for p in pts:
+            pos = (p[0], p[1])
+            cv2.circle(result_2, pos, 2, (0, 0, 255), 2)
+            cv2.putText(result_2, str(p), pos, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(255, 0, 0))
+
+        cv2.imshow("Moustache Filter", result_2)
+
+
+def face_swap_filter(cam):
+    # im1 = get_cam_frame(cam)
+    # im1 = cv2.resize(im1, (im1.shape[1] * SCALE_FACTOR, im1.shape[0] * SCALE_FACTOR))
+    # landmarks1 = get_landmarks(im1)
+
+    im1, landmarks1 = read_im_and_landmarks("resources/img1.png")
+    im2, landmarks2 = read_im_and_landmarks("resources/img2.png")
+
+    if type(landmarks1) is not int:
+        M = transformation_from_points(landmarks1[ALIGN_POINTS],
+                                       landmarks2[ALIGN_POINTS])
+
+        mask = get_face_mask(im2, landmarks2)
+        warped_mask = warp_im(mask, M, im1.shape)
+        combined_mask = np.max([get_face_mask(im1, landmarks1), warped_mask], axis=0)
+
+        warped_im2 = warp_im(im2, M, im1.shape)
+        warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
+
+        output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+        cv2.imshow("output", output_im)
+
+
 def main():
     cam = create_capture(0)
 
-    """
-    17 & 26 edges of left eye and right eye (left and right extrema)
-    0 & 16 edges of face across eyes (other left and right extra, interpolate between 0 & 17, 16 & 26 for half way pts)
-    19 & 24 top of left and right brows (top extremum)
-    27 is centre of the eyes on the nose (centre of glasses)
-    28 is the bottom threshold of glasses (perhaps interpolate between 27 & 28 if too low) (bottom extremum)
-    """
     glasses = cv2.imread('resources/glasses.png', -1)
+    moustache = cv2.imread('resources/moustache.png', -1)
 
     while True:
-        face = get_cam_frame(cam)
-        landmarks = get_landmarks(face)
-
-        # face, landmarks = read_im_and_landmarks(frame)
-        # annotate_landmarks(face, landmarks)
-
-        pts1 = np.float32([[0, 0], [599, 0], [0, 208], [599, 208]])
-
-        if type(landmarks) is not int:
-            print(landmarks[0, 0], landmarks[0, 1])
-            left_face_extreme = [landmarks[0, 0], landmarks[0, 1]]
-            right_face_extreme = [landmarks[16, 0], landmarks[16, 1]]
-            x_diff_face = right_face_extreme[0] - left_face_extreme[0]
-            y_diff_face = right_face_extreme[1] - left_face_extreme[1]
-
-            face_angle = math.degrees(math.atan2(y_diff_face, x_diff_face))
-
-            # get hypotenuse
-            face_width = math.sqrt((right_face_extreme[0] - left_face_extreme[0]) ** 2 +
-                                   (right_face_extreme[1] - right_face_extreme[1]) ** 2)
-            glasses_width = face_width * 1.0
-
-            # top and bottom of left eye
-            eye_height = math.sqrt((landmarks[19, 0] - landmarks[28, 0]) ** 2 +
-                                   (landmarks[19, 1] - landmarks[28, 1]) ** 2)
-            glasses_height = eye_height * 1.2
-
-            anchor_point = [landmarks[27, 0], landmarks[27, 1]]
-            tl = [int(anchor_point[0] - (glasses_width / 2)), int(anchor_point[1] - (glasses_height / 2))]
-            rot_tl = get_rotated_points(tl, anchor_point, face_angle)
-            print("tl", tl, rot_tl)
-
-            tr = [int(anchor_point[0] + (glasses_width / 2)), int(anchor_point[1] - (glasses_height / 2))]
-            rot_tr = get_rotated_points(tr, anchor_point, face_angle)
-            print("tr", tr, rot_tr)
-
-            bl = [int(anchor_point[0] - (glasses_width / 2)), int(anchor_point[1] + (glasses_height / 2))]
-            rot_bl = get_rotated_points(bl, anchor_point, face_angle)
-            print("bl", bl, rot_bl)
-
-            br = [int(anchor_point[0] + (glasses_width / 2)), int(anchor_point[1] + (glasses_height / 2))]
-            rot_br = get_rotated_points(br, anchor_point, face_angle)
-            print("br", br, rot_br)
-
-            pts = np.float32([rot_tl, rot_tr, rot_bl, rot_br])
-
-            M = cv2.getPerspectiveTransform(pts1, pts)
-
-            rotated = cv2.warpPerspective(glasses, M, (face.shape[1], face.shape[0]))
-            cv2.imwrite("rotated_glasses.png", rotated)
-
-            result_2 = blend_transparent(face, rotated)
-
-            for p in pts:
-                pos = (p[0], p[1])
-                cv2.circle(result_2, pos, 2, (0, 0, 255), 2)
-                cv2.putText(result_2, str(p), pos, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(255, 0, 0))
-
-            cv2.imshow("output", result_2)
-
-        if 0xFF & cv2.waitKey(1) == 27:
-            break
-
-            # cv2.imwrite("merged_transparent.png", result_2)
-
-    """
-    while True:
-        im1 = get_cam_frame(cam)
-        # im1 = cv2.resize(im1, (im1.shape[1] * SCALE_FACTOR, im1.shape[0] * SCALE_FACTOR))
-        # landmarks1 = get_landmarks(im1)
-
-        im_moustache = cv2.imread("./resources/moustache.png", -1)
-
-        # im1, landmarks1 = read_im_and_landmarks("ed.png")
-        # im2, landmarks2 = read_im_and_landmarks("./resources/im2.png")
-
-        result = blend_transparent(im1, im_moustache)
-        cv2.imshow("output", result)
-
-        if type(landmarks1) is not int:
-            im2_onto_im1 = face_swap(im1, landmarks1, im2, landmarks2)
-            # cv2.imwrite("output_2_onto_1.png", im2_onto_im1)
-            # temp_img = cv2.imread("output_2_onto_1.png", cv2.IMREAD_COLOR)
-            cv2.imshow("Output_2_to_1", im2_onto_im1)
-
-        if 0xFF & cv2.waitKey(1) == 27:
-            break
-
-        cv2.destroyAllWindows()
-
-
-        im1 = get_cam_frame(cam)
-        im1 = cv2.resize(im1, (im1.shape[1] * SCALE_FACTOR, im1.shape[0] * SCALE_FACTOR))
-        landmarks1 = get_landmarks(im1)
-
-        # im1, landmarks1 = read_im_and_landmarks("ed.png")
-        # im2, landmarks2 = read_im_and_landmarks("resources/beard.png")
-        # im2_onto_im1 = face_swap(im1, landmarks1, im2, landmarks2)
-
-        if type(landmarks1) is not int:
-            get_moustache_bounds(im1, landmarks1)
-            cv2.imshow("Frame", im1)
-
-
-        if 0xFF & cv2.waitKey(1) == 27:
+        # glasses_filter(cam, glasses)
+        # moustache_filter(cam, moustache)
+        face_swap_filter(cam)
+        if 0xFF & cv2.waitKey(30) == 27:
             break
 
     cv2.destroyAllWindows()
-    """
 
 
 if __name__ == '__main__':
