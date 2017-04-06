8f4f18f6940d879bb8591fae30fcc4205296e07c
Ed <oflynned@tcd.ie>
2017-04-06 21:03:51 +0100

    more ripping out of unnecessary code

--- main.py
+++ main.py
@@ -114,17 +114,19 @@ def warp_im(im, m, dshape):
 
 
 def correct_colours(im1, im2, landmarks1):
-    blur_amount = COLOUR_CORRECT_BLUR * np.linalg.norm(
-        np.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
-        np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+    mean_left = np.mean(landmarks1[LEFT_EYE_POINTS], axis=0)
+    mean_right = np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0)
+
+    blur_amount = COLOUR_CORRECT_BLUR * np.linalg.norm(mean_left - mean_right)
     blur_amount = int(blur_amount)
 
     if blur_amount % 2 == 0:
         blur_amount += 1
+
     im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
     im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
 
-    # Avoid division errors.
+    # avoid division errors
     im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
 
     return (im2.astype(np.float64) * im1_blur.astype(np.float64) /
--- video.py
+++ video.py
@@ -1,38 +1,4 @@
-from __future__ import print_function
-
 import cv2
-import numpy as np
-
-
-class VideoSynthBase(object):
-    def __init__(self, size=None, noise=0.0, bg=None):
-        self.bg = None
-        self.frame_size = (640, 480)
-        if bg is not None:
-            self.bg = cv2.imread(bg, 1)
-            h, w = self.bg.shape[:2]
-            self.frame_size = (w, h)
-
-        if size is not None:
-            w, h = map(int, size.split('x'))
-            self.frame_size = (w, h)
-            self.bg = cv2.resize(self.bg, self.frame_size)
-
-        self.noise = float(noise)
-
-    def read(self):
-        w, h = self.frame_size
-
-        if self.bg is None:
-            buf = np.zeros((h, w, 3), np.uint8)
-        else:
-            buf = self.bg.copy()
-
-        if self.noise > 0.0:
-            noise = np.zeros((h, w, 3), np.int8)
-            cv2.randn(noise, np.zeros(3), np.ones(3) * 255 * self.noise)
-            buf = cv2.add(buf, noise, dtype=cv2.CV_8UC3)
-        return True, buf
 
 
 def create_capture(source=0):
